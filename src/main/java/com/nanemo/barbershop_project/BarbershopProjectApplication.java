8428766235e3084b887d68378bd9495af624c1da
Nadir Nadirli <companynadirnadirli@gmail.com>
2024-01-29 22:18:24 +0400

    start to project

---
+++ src/main/java/com/nanemo/barbershop_project/BarbershopProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nanemo.barbershop_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbershopProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarbershopProjectApplication.class, args);
+    }
+
+}
